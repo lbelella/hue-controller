a6d9d614489d70a2f7343f6e430b4eeda80f5779
LB Belella <lb.belella@live.com>
2016-12-24 09:43:23 -0500

    - Refactored the way lights are controlled.  Now each light is assigned to a HueWidget. - HueWidget now controls on/off, color, saturation and brightness - Currently only works if lights are set to hs color mode

--- hue_control.py
+++ hue_control.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
 import sys
 from phue import Bridge
 
@@ -11,6 +11,89 @@ class HueController():
         self.bridge = Bridge('192.168.2.102')
         self.bridge.connect()
         self.lights = self.bridge.get_light_objects()
+        
+class HueWidget(QtGui.QWidget):
+    def __init__(self, light):
+        super(HueWidget, self).__init__()
+        
+        print(light.name + " " + light.colormode)
+        
+        self.light = light
+        self.displayName = light.name
+        
+        self.initUi()
+    
+    def initUi(self):
+        # Layout our controls horizontally
+        self.layout = QtGui.QHBoxLayout()
+        self.layout.addStretch(1)
+        
+        # On/Off Toggle Button
+        self.btnOnOff = QtGui.QPushButton(self.displayName)
+        self.btnOnOff.setMinimumSize(200, 1)
+        self.btnOnOff.clicked.connect(lambda: self.toggleLight())
+        
+        # Color slider
+        self.sldColor = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.sldColor.setMinimumSize(200, 1)
+        self.sldColor.setTracking(False)
+        self.sldColor.setMinimum(0)
+        self.sldColor.setMaximum(65535)
+        self.sldColor.setValue(self.light.hue)
+        self.sldColor.valueChanged.connect(lambda: self.setColor(self.sldColor.value()))
+        
+        # Brightness slider
+        self.sldBrightness = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.sldColor.setMinimumSize(200, 1)
+        self.sldBrightness.setTracking(False)
+        self.sldBrightness.setMinimum(0)
+        self.sldBrightness.setMaximum(254)
+        self.sldBrightness.setValue(self.light.brightness)
+        self.sldBrightness.valueChanged.connect(lambda: self.setBrightness(self.sldBrightness.value()))
+        
+        # Saturation slider
+        self.sldSat = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.sldColor.setMinimumSize(200, 1)
+        self.sldSat.setTracking(False)
+        self.sldSat.setMinimum(0)
+        self.sldSat.setMaximum(254)
+        self.sldSat.setValue(self.light.saturation)
+        self.sldSat.valueChanged.connect(lambda: self.setSaturation(self.sldSat.value()))
+        
+        # Add the controls to the horizontal layout
+        self.layout.addWidget(self.btnOnOff)
+        self.layout.addWidget(self.sldColor)
+        self.layout.addWidget(self.sldBrightness)
+        self.layout.addWidget(self.sldSat)
+        
+        # Set the layout for the widget to the horizontal layout
+        self.setLayout(self.layout)
+    
+    def setSaturation(self, sat):
+        self.light.on = True
+        self.light.saturation = sat
+        
+    def setColor(self, color):
+        self.light.on = True
+        self.light.hue = color
+        
+    def setBrightness(self, brightness):
+        """
+        Turns on the light and sets the brightness to the value
+        that was passed in.
+        """
+        self.light.on = True
+        self.light.brightness = brightness
+        
+    def toggleLight(self):
+        """
+        Toggles the on/off state of the light that was passed into us.
+        """
+        if self.light.on == True:
+            self.light.on = False
+        else:
+            self.light.on = True
+        return
         
 class HueControlApp(QtGui.QMainWindow):
     def __init__(self, hc):
@@ -31,13 +114,12 @@ class HueControlApp(QtGui.QMainWindow):
         self.mainLayout = QtGui.QVBoxLayout()
         self.mainLayout.addStretch(1)
         
-        # Create a list of buttons using each light on the bridge
-        buttons = [QtGui.QPushButton(l.name) for l in self.hc.lights]
-        
-        # Wire up the click handler and add to main layout
-        for b in buttons:
-            b.clicked.connect(lambda a, b=b: self.toggle_light(b.text()))
-            self.mainLayout.addWidget(b)
+        # Create a list of widgets using each light on the bridge
+        widgets = [HueWidget(l) for l in self.hc.lights]
+
+        # Add each widget to the main layout
+        for w in widgets:
+            self.mainLayout.addWidget(w)
         
         # Central widget
         self.centralWidget = QtGui.QWidget()
@@ -45,19 +127,6 @@ class HueControlApp(QtGui.QMainWindow):
 
         # Set central widget
         self.setCentralWidget(self.centralWidget)
-            
-    def toggle_light(self, name):
-        """
-        Toggles the on/off state of the light identified by name.
-        Since the light buttons were created using the light names
-        directly from the bridge this should be OK.
-        """
-        light = self.hc.bridge[name]
-        if light.on == True:
-            light.on = False
-        else:
-            light.on = True
-        return
         
 def main():
     app = QtGui.QApplication(sys.argv)
